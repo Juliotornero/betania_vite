6c3c480e07020dc4eca1a4aba39c971d987ebe16
julio <juliotornero8@gmail.com>
2023-10-01 15:26:35 -0500

    Solucion a problemas de resize

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,59 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [pantallaTablet, setpantallaTablet] = useState(false);
-  const [pantallaDesktop, setpantallaDesktop] = useState(false);
+  const [pantallaTablet, setPantallaTablet] = useState(false);
+  const [pantallaDesktop, setPantallaDesktop] = useState(false);
   
 
-    const handleTablet = ()=>{
-        if(window.innerWidth >= 768){
-            setpantallaTablet(true);
-        }else{
-            setpantallaTablet(false);
-        }
-    }
+    // const handleTablet = ()=>{
+    //     if(window.innerWidth >= 768){
+    //         setpantallaTablet(true);
+    //     }else{
+    //         setpantallaTablet(false);
+    //     }
+    // }
 
-    useEffect(() => {
-        window.addEventListener('resize', handleTablet);
+    // useEffect(() => {
+    //     window.addEventListener('resize', handleTablet);
     
-        return () => {
-            window.removeEventListener('resize', handleTablet);
-        };
-      }, []);
+    //     return () => {
+    //         window.removeEventListener('resize', handleTablet);
+    //     };
+    //   }, []);
 
-    const handleDesktop = ()=>{
-        if(window.innerWidth >= 1024){
-            setpantallaDesktop(true);
-        }else{
-            setpantallaDesktop(false);
-        }
-    }
+    // const handleDesktop = ()=>{
+    //     if(window.innerWidth >= 1024){
+    //         setpantallaDesktop(true);
+    //     }else{
+    //         setpantallaDesktop(false);
+    //     }
+    // }
 
-    useEffect(() => {
-        window.addEventListener('resize', handleDesktop);
+    // useEffect(() => {
+    //     window.addEventListener('resize', handleDesktop);
     
-        return () => {
-            window.removeEventListener('resize', handleDesktop);
-        };
-      }, []);
+    //     return () => {
+    //         window.removeEventListener('resize', handleDesktop);
+    //     };
+    //   }, []);
+
+    const handleResize = () => {
+      const isTablet = window.innerWidth >= 768;
+      const isDesktop = window.innerWidth >= 1024;
+      setPantallaTablet(isTablet);
+      setPantallaDesktop(isDesktop);
+    };
+  
+    useEffect(() => {
+      window.addEventListener('resize', handleResize);
+  
+      // Llama a la función de detección de tamaño inicialmente
+      handleResize();
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []); 
 
   return (
     <>
