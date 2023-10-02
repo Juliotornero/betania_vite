81145cacd42f26e0aea7e54863dc19151c1a17af
julio <juliotornero8@gmail.com>
2023-10-02 10:25:41 -0500

    Fixeo de menu II

--- src/components/headeranterior.jsx
+++ src/components/headeranterior.jsx
@@ -33,13 +33,12 @@ const Header = ({pantallaTablet, pantallaDesktop}) => {
 
       
     const openMenu = () => {
-        console.log('Abriendo')
+        console.log('Ejecutando')
         setIsMenuOpen(true);
         document.body.classList.add('overflow-hidden');
       };
       
     const closeMenu = () => {
-        console.log('Cerrando')
         setIsMenuOpen(false);
         document.body.classList.remove('overflow-hidden');
       };
@@ -57,10 +56,10 @@ const Header = ({pantallaTablet, pantallaDesktop}) => {
                         {scrollDown 
                             ? 
                             <>
-                                <button onClick={openMenu} className='flex justify-center items-center text-black'>
-                                    <img className='w-8 desktop:w-10' src={imgMenu} alt="menuIcon"/>
+                                <a href='#' className='flex justify-center items-center text-black'>
+                                    <img onClick={openMenu} className='w-8 desktop:w-10' src={imgMenu} alt="menuIcon"/>
                                     {pantallaTablet && <span className='ml-2 uppercase text-sm pr-14'>Menu</span>}
-                                </button>
+                                </a>
                                                                 
                                 {pantallaTablet && <span><img className='tablet:w-[150px] desktop:w-[200px]' src={logoName} alt="Logo Be'Tania"/></span>}
 
@@ -68,9 +67,10 @@ const Header = ({pantallaTablet, pantallaDesktop}) => {
 
                                     {!pantallaTablet && <button><img className='w-12 mr-2' src={logoBlack} alt="Logo Be'Tania"/></button>}
                                     
-                                    <button className={`w-[134px] h-[38px] bg-[#181818] border-2  border-black text-white uppercase text-sm font-work rounded hover:bg-transparent hover:text-black 
+                                    <button className={`h-[38px] w-[134px] uppercase text-sm font-work rounded hover:bg-transparent hover:text-white border-2 
                                     tablet:h-[48px] tablet:w-[167px] 
-                                    desktop:w-[199px] desktop:h-[58px]`}
+                                    desktop:w-[199px] desktop:h-[58px] 
+                                    ${scrollDown ? 'bg-[#181818] text-white hover:text-gray-900 border-gray-950' : 'bg-white text-black'}`}
                                     >
                                         Reserva
                                     </button>
@@ -80,44 +80,50 @@ const Header = ({pantallaTablet, pantallaDesktop}) => {
                             </>
                             : 
                             <>
-                                <button onClick={openMenu} className='flex justify-center items-center text-white'>
-                                    <img className='w-8 desktop:w-10' src={imgMenuWhite} alt="menuIcon"/>
+                                <a href='#'  className='flex justify-center items-center text-white'>
+                                    <img onClick={openMenu} className='w-8 desktop:w-10' src={imgMenuWhite} alt="menuIcon"/>
                                     {pantallaTablet && <span className='ml-2 uppercase text-sm'>Menu</span>}
-                                </button>
+                                </a>
 
-                                <button className={`w-[134px] h-[38px] uppercase font-work text-sm  rounded bg-white text-black hover:bg-transparent hover:text-white border-2 
+                                <button className={`h-[38px] w-[134px] uppercase text-sm font-work px-5 py-1 rounded hover:bg-transparent hover:text-white border-2 
                                 tablet:h-[48px] tablet:w-[167px] 
                                 desktop:w-[199px] desktop:h-[58px] 
-                                `}
+                                ${scrollDown ? 'bg-[#181818] text-white hover:text-gray-900 border-gray-950' : 'bg-white text-black'}`}
                                 >
                                     Reserva
                                 </button>
 
-                            </>       
+                            </>
+                               
+
+                                
                         }               
                     
                         
                     </div>
                     
-                    <nav className={`w-screen h-screen absolute top-0 left-0 z-50 px-4 py-4 text-white bg-[#181818] ${!isMenuOpen && 'hidden'}`}>
-
-                        <div className='flex flex-row-reverse justify-between'>
-                            <button onClick={closeMenu}><img className='w-8 mr-5 mt-5' src={imgClose} alt="closeIcon"/></button>
-                        </div>
-
-                        <div className='flex flex-col text-xl mx-auto max-w-screen-desktop tablet:text-2xl desktop:text-4xl'>
-                            <span className='mt-20 mb-16 text-center p-2'><img className='w-16 desktop:w-28' src={logoWhite} alt="" /></span>
-                            <a href="#" className='my-3 hover:text-[#89745C]'>HABITACIONES</a>
-                            <a href="#" className='my-3 hover:text-[#89745C]'>SERVICIOS</a>
-                            <a href="#" className='my-3 hover:text-[#89745C]'>ATRACCIONES EN PISCO</a>
-                            <a href="#" className='my-3 hover:text-[#89745C]'>PREGUNTAS PRECUENTES</a>
-                            <a href="#" className='my-3 hover:text-[#89745C]'>CONTACTO</a>
-                            <button className='uppercase mt-10 px-5 py-2 w-[190px] text-sm font-work rounded border-2 bg-transparent hover:bg-white hover:text-black tablet:text-2xl desktop:text-2xl desktop:w-[200px]'>Reserva</button>
-                        </div>
-
-                    </nav>
 
                 </div>
+
+
+                <nav className={`w-full h-screen absolute z-50 px-4 py-4 text-white bg-[#181818] top-0 left-0 ${!isMenuOpen && 'hidden'}`}>
+
+
+                    <div className='flex flex-row-reverse justify-between'>
+                        <a href='#' onClick={closeMenu}><img className='w-8 mr-5 mt-5' src={imgClose} alt="closeIcon"/></a>
+                    </div>
+                    
+                    <div className='flex flex-col text-xl mx-auto max-w-screen-desktop tablet:text-2xl desktop:text-4xl'>
+                        <a className='mt-20 mb-16 text-center p-2' href="#"><img className='w-16 desktop:w-28' src={logoWhite} alt="" /></a>
+                        <a href="#" className='my-3 hover:text-[#89745C]'>HABITACIONES</a>
+                        <a href="#" className='my-3 hover:text-[#89745C]'>SERVICIOS</a>
+                        <a href="#" className='my-3 hover:text-[#89745C]'>ATRACCIONES EN PISCO</a>
+                        <a href="#" className='my-3 hover:text-[#89745C]'>PREGUNTAS PRECUENTES</a>
+                        <a href="#" className='my-3 hover:text-[#89745C]'>CONTACTO</a>
+                        <button className='uppercase mt-10 px-5 py-2 w-[190px] text-sm font-work rounded border-2 bg-transparent hover:bg-white hover:text-black tablet:text-2xl desktop:text-2xl desktop:w-[200px]'>Reserva</button>
+                    </div>
+
+                </nav>
 
                 <div className='absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center w-full'>
                     <img className='w-[300px] tablet:w-[450px] desktop:w-[550px]' src={logoNameWhite} alt="Logo Be'Tania"/>
