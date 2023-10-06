e57706f2f1060a41a3377bca9d9c14c3644bf365
julio <juliotornero8@gmail.com>
2023-10-06 15:09:52 -0500

    Parte II

--- src/components/SectionDiscover.jsx
+++ src/components/SectionDiscover.jsx
@@ -20,7 +20,7 @@ const SectionDiscover = ({pantallaTablet}) => {
         {pantallaTablet 
         
           ? 
-            <div className='mt-10 flex flex-row max-w-screen-desktop'>
+            <div className='mt-10 flex flex-row max-w-screen-desktop mx-auto'>
                   
               
                 <article className="relative w-full h-[480px] flex group overflow-hidden mx-2">
@@ -141,7 +141,7 @@ const SectionDiscover = ({pantallaTablet}) => {
                             className="w-full h-full transition-transform transform scale-100 group-hover:scale-105"
                           />
                           <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-evenly">
-                          <p className="text-white text-3xl font-bold tracking-wide">Pisco</p>
+                          <p className="text-white text-3xl font-bold tracking-wide">Descubre Pisco</p>
                           <DiscoverButton/>
                           </div>
                       </article>
